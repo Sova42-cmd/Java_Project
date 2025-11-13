162873bfb890a4e3cf1b33d5ed23d2e5b1833f97
Sova42-cmd <steamsova42@gmail.com>
2025-11-13 17:31:12 +0400

    basics101

--- src/main.java
+++ src/main.java
@@ -1,7 +1,27 @@
 public class main {
-
     public static void main(String[] args){
-        System.out.println("Hi, It's Sova42's first code!");
-        System.out.println("Sova42's second code!");
+
+       // There are 4 main data types in Java
+         // Int – integer number, same as in Python.
+         // Double – Decimal number, same as Python's Float data type.
+         // char – A character, e.g.  #$%
+         // boolean – true/false state.
+
+        int age = 42; //always end with semi-column ;
+        double gpa = 42.5;
+        char symbol = '&'; //in single brackets
+        boolean isOnline = true;
+
+//        System.out.println(age);
+//        System.out.println(gpa);
+//        System.out.println(symbol);
+//        System.out.println(isOnline);
+
+        if (isOnline) {
+            System.out.println("User is online!");
+        }
+        else{
+            System.out.println("User is offline");
+        }
     }
 }
