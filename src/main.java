fba95019e45b4a1a0ae0afa80232000643a2c2a0
Sova42-cmd <steamsova42@gmail.com>
2025-11-13 16:18:46 +0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String[] args){
+        System.out.println("Hi, It's Sova42's first code!");
+        System.out.println("Sova42's second code!");
+    }
+}
