5498b8b1befc906cc59a94358d0ce780e77bfac5
Sova42-cmd <steamsova42@gmail.com>
2025-11-14 18:05:19 +0400

    basics103

--- src/main.java
+++ src/main.java
@@ -1,20 +1,52 @@
 import java.util.Scanner;
 
-public class main {
+public class main{
 
     public static void main(String[] args){
 
+//        Scanner scanner = new Scanner(System.in);
+
+            // HYPOTENUSE
+//
+//        double a;
+//        double b;
+//        double c;
+//
+//        System.out.println("Enter the lenght of first page: ");
+//        a = scanner.nextDouble();
+//
+//        System.out.println("Enter the lenght of second page: ");
+//        b = scanner.nextDouble();
+//
+//        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//
+//        System.out.println("The hypotenuse is: " + c);
+//
+//        scanner.close();
+
+        // Circumference -> 2πr
+        // Area -> πr^2
+        // Volume -> 4/3πr^3
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Adult Website: Are you 18+ (y/n)?:  ");
-        String confirmation = scanner.nextLine();
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        if(confirmation.equals("y")){
-            System.out.println("Welcome to pornhub!");
-        } else{
-            System.out.println("No porn for you kiddo ;<");
-        }
+        System.out.println("Enter the radius of the circle: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2*Math.PI*radius;
+        area = Math.PI*Math.pow(radius, 2);
+        volume = (4.0 / 3.0)*Math.PI*Math.pow(radius,3);
+
+        System.out.printf("The circumference of the circle is... → %.1f\n", circumference);
+        System.out.printf("The area of the circle is... → %.1f\n", area);
+        System.out.printf("The volume of the circle is... → %.1f\n", volume);
 
         scanner.close();
     }
+
 }
