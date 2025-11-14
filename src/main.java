d9d85d66978dd1c832fcaaef51cd34dd6f7b69c5
Sova42-cmd <steamsova42@gmail.com>
2025-11-14 12:34:41 +0400

    basics103 (Shopping cart program)

--- src/main.java
+++ src/main.java
@@ -1,58 +1,45 @@
 import java.util.Scanner;
 
-public class main {
-
+public class main{
     public static void main(String[] args){
 
-//        Scanner scanner = new Scanner(System.in);
-//
-//        double height = 0;
-//        double widht = 0;
-//        double area = 0;
-//
-//        System.out.println("Enter the height's size: ");
-//        height = scanner.nextDouble();
-//
-//        System.out.println("Enter the width's size: ");
-//        widht = scanner.nextDouble();
-//
-//        area = height*widht;
-//
-//        System.out.println("The area of your rectangle is: " + area);
-//
-//        scanner.close();
+        System.out.println("|______________|");
+        System.out.println("Price List:");
+        System.out.println("Item1 – $19.99");
+        System.out.println("Item2 – $29.99");
+        System.out.println("Iten3 – $49.99");
+        System.out.println("Item4 – $99.99");
+        System.out.println("|______________|");
+
+        double item1 = 19.99;
+        double item2 = 29.99;
+        double item3 = 49.99;
+        double item4 = 99.99;
 
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1;
-        String adjective2;
-        String noun1;
-        String noun2;
-        String verb1;
+        System.out.println("What would you like to buy? (item1/item2/item3/item4):");
+        String items = scanner.nextLine();
 
-        System.out.println("Enter a place name: ");
-        noun1 = scanner.nextLine();
+        System.out.println("How many would you like?: ");
+        double quantity = scanner.nextDouble();
 
-        System.out.println("Enter a person's name: ");
-        noun2 = scanner.nextLine();
+        System.out.println("Your cart: " + items + " quantity: " + quantity);
 
-        System.out.println("Enter the first adjective: ");
-        adjective1 = scanner.nextLine();
+        if(items.equals("item1")){
+            System.out.println("price: " + item1 * quantity);
+        } else if(items.equals("item2")){
+            System.out.println("price: " + item2 * quantity);
+        } else if(items.equals("item3")){
+            System.out.println("price: " + item3 * quantity);
+        } else if(items.equals("item4")){
+            System.out.println("price: " + item4 * quantity);
+        } else {
+            System.out.println("Something went wrong");
+        }
 
-        System.out.println("Enter the second adjective: ");
-        adjective2 = scanner.nextLine();
-
-        System.out.println("Enter a verb: ");
-        verb1 = scanner.nextLine();
-
-        System.out.println("Today I went to the " + noun1 + " and met there " + noun2 +
-        " He/She was really " + adjective1 + " and rather " + adjective2 +
-                " After that I " + verb1 + " and died.");
-
-        //sorry for mid quality
 
         scanner.close();
 
     }
-
 }
