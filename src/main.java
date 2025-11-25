9cb94644b7b5a45c93e5578de0e0e2582deb4dfa
Sova42-cmd <steamsova42@gmail.com>
2025-11-25 10:33:31 +0400

    Monocraft font experiments #1

--- src/main.java
+++ src/main.java
@@ -1,34 +1,6 @@
-import java.util.Scanner;
-
 public class main{
-
     public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-
-            double initial;
-            double rate;
-            double timesCompounded;
-            int years;
-            double finalAmount;
-
-        System.out.println("Enter an inital amount: ");
-        initial = scanner.nextDouble();
-
-        System.out.println("Enter the % rate: ");
-        rate = scanner.nextDouble() / 100;
-
-        System.out.println("Enter the amount of times compounded yearly: ");
-        timesCompounded = scanner.nextDouble();
-
-        System.out.println("Enter the amount of years: ");
-        years = scanner.nextInt();
-
-        finalAmount = initial * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
-
-        System.out.printf("You'll have $%.2f in %d", finalAmount, years);
-
-        scanner.close();
+        System.out.println("Heeyy, my first Monocraft text");
+        System.out.println("Wonder whether It'll work in Git repository");
     }
-
 }
